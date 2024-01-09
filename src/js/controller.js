84b41dce0d924429db52c2a200a918e55944b6e9
Kamil Justyński <kamil.justynski1998@gmail.com>
2024-01-09 19:10:55 +0100

    implementing search results part one

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,8 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { ERROR_MESS } from './config.js';
 
 const controlRecipes = async function () {
   try {
@@ -20,7 +20,19 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+    await model.loadSearchResults(query);
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
