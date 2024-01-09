e1a2aa81264e7dec48441b05efb0da6c992eb55a
Kamil Justyński <kamil.justynski1998@gmail.com>
2024-01-09 18:32:53 +0100

    Added helpers and configuration files, event handlers in MVC- publisher subscriber pattern, implementing error and success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import { ERROR_MESS } from './config.js';
 
 const controlRecipes = async function () {
   try {
@@ -23,10 +16,11 @@ const controlRecipes = async function () {
 
     recipeView.render(model.state.recipe);
   } catch (err) {
-    new Error(console.error(err.message));
+    recipeView.renderError();
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,18 +1,15 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const response = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
+    const data = await getJSON(`${API_URL}/${id}`);
 
-    const data = await response.json();
-
-    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
-
-    const { recipe } = await data.data;
+    const { recipe } = data.data;
 
     state.recipe = {
       id: recipe.id,
@@ -26,5 +23,6 @@ export const loadRecipe = async function (id) {
     };
   } catch (err) {
     new Error(console.error(err));
+    throw err;
   }
 };
