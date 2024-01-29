168af3c43cffc052f09755b93492ea3ac1f07f11
Kamil Justyński <kamil.justynski1998@gmail.com>
2024-01-29 19:40:16 +0100

    Adding functionality for creating and submitting new recipes

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'feaf023b-43c2-435d-89cd-7b8d6984a672';
+export const MODAL_CLOSE_SEC = 2.5;
